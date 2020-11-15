8b1b5905d310d3cd5a187d7c3c13b016df95005b
k4keye <vps32@naver.com>
2020-11-15 23:22:00 +0900

    enum,null

--- src/main/java/com/example/demo/jpa/User.java
+++ src/main/java/com/example/demo/jpa/User.java
@@ -11,17 +11,18 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 public class User {
-    @Id
+    @Id //PK 지정
     private String id;
 
-    @Column(name ="UserPwd")
+    @Column(name ="UserPwd" , nullable = false) //컬럼 UserPwd 에 변수 pwd 를 매핑한다 , null을 허용하지않는다.
     private String pwd;
-    @Column(name ="UserName")
+    @Column(name ="UserName" , nullable = false)
     private String name;
     @Column(name ="UserEmail")
     private String email;
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) //enum 을 적용시키는데 저장할때 문자열로 저장한다.
     private MemberType memberType;
+
     @Builder
     public User(String id , String pwd ,String name, String email,MemberType memberType){
         this.id = id ;
